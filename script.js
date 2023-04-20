72787c68e46041a0ed1207553325eac2682d7cdd
unknown <Jsslchrs@gmail.com>
2023-04-20 15:25:27 +0800

    initial commit

--- script.js
+++ script.js
@@ -12,14 +12,18 @@ if (number < 100) {
 } else {
 	alert("The number you entered is 100 or greater.");
 }
-
 for (let i = 0; i <= 30; i++) {
-	console.log(i);
-}
+	document.write(i + "<br>");
+  }
+  
+for (let i = 0; i <= 40; i += 2) {
+	document.write(i + "<br>");
+  }
 
-for (let i = 40; i >= 0; i -= 2) {
-	if (i % 3 === 0) {
-		console.log(i);
-	}
-}
+ for (let i = 40; i >= 10; i--) {
+        if (i % 3 == 0) {
+          document.write(i + "<br>");
+        }
+      }
+	  
 
